e79bd56f56ef21da37e30a208b2f584df3f79aef
Marcel Fiore <marcelaf@rams.colostate.edu>
2018-11-19 22:25:59 -0700

    fix errors after removing server

--- src/main/java/com/marcelfiore/jungle_ai/game/gui/GUI.java
+++ src/main/java/com/marcelfiore/jungle_ai/game/gui/GUI.java
@@ -17,30 +17,11 @@ public class GUI extends JFrame {
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Background b = new Background("src/Images/jungle.jpg");
-        add(b);
-
         currentPanel = new MainMenuPage(this);
         add(currentPanel);
 
         pack();
         setVisible(true);
-    }
-
-    public void changePage(String panelName) {
-        switch (panelName) {
-            case "StartGame":
-                remove(currentPanel);
-                currentPanel = new GamePage(this);
-                add(currentPanel);
-                break;
-
-            default:
-                break;
-        }
-
-        revalidate();
-        repaint();
     }
 
     public void changePageTo(JPanel comp) {
@@ -50,32 +31,6 @@ public class GUI extends JFrame {
         revalidate();
         repaint();
         pack();
-    }
-    public void startGUI(GUI g, Socket client){
-        //javax.swing.SwingUtilities.invokeLater(() -> g.createAndShowGUI());
-        final JFrame frame = new JFrame("Jungle");
-        final JButton btnLogin = new JButton("Login");
-        final JButton btnRegister = new JButton("Register");
-        btnLogin.addActionListener(
-                new ActionListener(){
-                    public void actionPerformed(ActionEvent e) {
-                        LoginDialog loginDlg = new LoginDialog(frame, client);
-                        loginDlg.setVisible(true);
-                        // if logon successfully
-                        if(loginDlg.isSucceeded()){
-                            //btnLogin.setText("Hi " + loginDlg.getUsername() + "!");
-                            javax.swing.SwingUtilities.invokeLater(() -> g.createAndShowGUI());
-                        }
-                    }
-                });
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300, 100);
-        frame.setLayout(new FlowLayout());
-        frame.getContentPane().add(btnLogin);
-        frame.getContentPane().add(btnRegister);
-        frame.setVisible(true);
-
     }
 
     public static void main(String[] args) {
