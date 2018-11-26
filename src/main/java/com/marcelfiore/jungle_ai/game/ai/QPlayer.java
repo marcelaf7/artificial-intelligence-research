46064bbdd77beaa93e03a2e03a3263d90268fadd
Marcel Fiore <marcelaf@rams.colostate.edu>
2018-11-25 22:00:35 -0700

    fixed compile errors

--- src/main/java/com/marcelfiore/jungle_ai/game/ai/QPlayer.java
+++ src/main/java/com/marcelfiore/jungle_ai/game/ai/QPlayer.java
@@ -1,19 +1,21 @@
 package com.marcelfiore.jungle_ai.game.ai;
 
 import java.util.Dictionary;
+import com.marcelfiore.jungle_ai.game.jungle.Location;
+
 
 public class QPlayer implements AIPlayer {
-  private Dictionary<Location[], Location[]> qTable;
+  public QDatabase db;
 
-  public void train(numGames, learningRate, epsilonDecayFactor) {
+  public void train(int numGames, double learningRate, double epsilonDecayFactor) {
 
   }
 
   public int[] getMove(Location[] gameState, Location[] validMoves) {
-
+    return null;
   }
 
   private Location[] epsilonGreedy(double epsilon, Location[] state) {
-
+    return null;
   }
 }
--- src/main/java/com/marcelfiore/jungle_ai/game/ai/RandomPlayer.java
+++ src/main/java/com/marcelfiore/jungle_ai/game/ai/RandomPlayer.java
@@ -1,14 +1,16 @@
 package com.marcelfiore.jungle_ai.game.ai;
 
+import com.marcelfiore.jungle_ai.game.jungle.Location;
+
 public class RandomPlayer implements AIPlayer {
 
   public int[] getMove(Location[] gameState, Location[] validMoves) {
-    int randomMove = Math.floor((Math.random() * validMoves.length) / 2) * 2;
-    return [
+    int randomMove = (int) Math.floor((Math.random() * validMoves.length) / 2) * 2;
+    return new int[] {
       validMoves[randomMove].getCol(),
       validMoves[randomMove].getRow(),
       validMoves[randomMove + 1].getCol(),
       validMoves[randomMove + 1].getCol()
-    ];
+    };
   }
 }
