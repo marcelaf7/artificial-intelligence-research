4749d4b7926dc6eae8e112a0b7abb6c939309b82
Marcel Fiore <marcelaf@rams.colostate.edu>
2018-12-08 23:12:37 -0700

    started train method

--- src/main/java/com/marcelfiore/jungle_ai/game/ai/QPlayer.java
+++ src/main/java/com/marcelfiore/jungle_ai/game/ai/QPlayer.java
@@ -2,13 +2,34 @@ package com.marcelfiore.jungle_ai.game.ai;
 
 import java.util.Dictionary;
 import com.marcelfiore.jungle_ai.game.jungle.Location;
+import com.marcelfiore.jungle_ai.game.jungle.Game;
 import com.marcelfiore.jungle_ai.game.gui.AITrainPage;
 
 
 public class QPlayer implements AIPlayer {
   public QDatabase db;
 
+  public QPlayer() {
+    db = new QDatabase();
+  }
+
   public void train(int numGames, double learningRate, double epsilonDecayFactor, AITrainPage page) {
+    double epsilon = 1.0;
+    boolean graphics = page != null;
+
+    Game game = new Game();
+
+    for (int games = 0; games < numGames; games++) {
+      boolean done = false;
+
+      while (!done) {
+        String move = epsilonGreedy(epsilon, game.getState());
+
+
+
+
+      }
+    }
 
   }
 
@@ -16,7 +37,7 @@ public class QPlayer implements AIPlayer {
     return null;
   }
 
-  private Location[] epsilonGreedy(double epsilon, Location[] state) {
+  private String epsilonGreedy(double epsilon, String state) {
     return null;
   }
 }
